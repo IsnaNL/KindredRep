b584c5a4106fd52d34b5da909d0bf9e3c62048e1
Amir Aharoni <Aharoniamirr@gmail.com>
2021-01-07 16:27:01 +0200

    "shadow"

---
+++ Assets/CastShadowDown.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CastShadowDown : MonoBehaviour
+{
+    public Transform shadowGFX;
+    public LayerMask groundlayer;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 10f, groundlayer);
+        if (hit)
+        {
+            shadowGFX.position = hit.point;
+        }
+    }
+}
