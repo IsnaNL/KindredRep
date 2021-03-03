05553a34bb54a0390a0ade452d3ffae9e274212b
Amir Aharoni <Aharoniamirr@gmail.com>
2021-03-03 22:29:27 +0200

    The gate is great
    no graphics for gate
    but yet
    the gate is still great
    it works with states
    
    sometimes it breaks
    sometimes it shakes
    yet still great
    the gate

---
+++ Assets/GatePrefab.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class GatePrefab : MonoBehaviour
+{
+    private enum GateMode
+    {
+        OnTriggerEnter, OnTriggerExit, OnLivebodyKilled
+    }
+    private Animator myAnimator;
+    [SerializeField]private bool IsOpen;
+    private bool executeLiveBodyCheck;
+    [SerializeField] private GateMode OpenMode;
+    [SerializeField] private GateMode CloseMode;
+    public GameObject liveBodyRef;
+    void Start()
+    {
+        myAnimator = GetComponent<Animator>();
+        executeLiveBodyCheck = liveBodyRef != null && 
+            (OpenMode == GateMode.OnLivebodyKilled || CloseMode == GateMode.OnLivebodyKilled);
+
+        myAnimator.SetBool("IsOpen", IsOpen);
+    }
+    private void Update()
+    {
+        if (executeLiveBodyCheck)
+        {
+            if (!liveBodyRef.activeInHierarchy)
+            {
+                TriggerMode(GateMode.OnLivebodyKilled);
+            }
+        }
+        
+    }
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            TriggerMode(GateMode.OnTriggerEnter);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            TriggerMode(GateMode.OnTriggerExit);
+        }
+    }
+    private void Open()
+    {
+        if (IsOpen)
+        {
+            return;
+        }
+        IsOpen = true;
+        myAnimator.SetBool("IsOpen", IsOpen);
+    }
+    private void Close()
+    {
+        if (!IsOpen)
+        {
+            return;
+        }
+        IsOpen = false;
+        myAnimator.SetBool("IsOpen", IsOpen);
+    }
+    private void TriggerMode(GateMode Context)
+    {
+        if (OpenMode == Context)
+        {
+            Open();
+        }
+        else if (CloseMode == Context)
+        {
+            Close();
+        }
+    }
+}
